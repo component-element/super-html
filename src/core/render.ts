2e33195b9c9623dad369e50f5ca5cb4ec377f6d4
wanglin.john <wanglin.john@bytedance.com>
2019-10-08 18:21:33 +0800

    #0.0.1: 删除旧代码，梳理目录结构

--- src/core/render.ts
+++
@@ -1,5 +0,0 @@
-import { render, TemplateResult, RenderOptions } from 'lit-html';
-
-export default function(result: TemplateResult, container: Element | DocumentFragment, options?: Partial<RenderOptions>) {
-    return render(result, container, options);
-}
